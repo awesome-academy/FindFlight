6a733df63fc6e32ffd5d45c0c640c404536c3822
Vinh Tran <vinhmone@gmail.com>
2021-08-03 14:07:56 +0700

    Create UI OfferFlightsListScreen

---
+++ app/src/main/java/com/sun/findflight/ui/offerflightslist/OfferFlightsListContract.kt
@@ -0,0 +1,17 @@
+package com.sun.findflight.ui.offerflightslist
+
+import com.sun.findflight.base.BasePresenter
+import com.sun.findflight.base.BaseView
+import com.sun.findflight.data.model.BasicFlight
+import com.sun.findflight.data.model.FlightDetail
+
+interface OfferFlightsListContract {
+    interface View : BaseView {
+        fun showOfferFlights(flights: List<FlightDetail>)
+    }
+
+    interface Presenter : BasePresenter {
+        fun getOfferFlights(basicFlight: BasicFlight)
+        fun getOfferFlightsByUrl(url: String, basicFlight: BasicFlight)
+    }
+}
---
+++ app/src/main/java/com/sun/findflight/ui/offerflightslist/OfferFlightsListPresenter.kt
@@ -0,0 +1,15 @@
+package com.sun.findflight.ui.offerflightslist
+
+import com.sun.findflight.data.model.BasicFlight
+
+class OfferFlightsListPresenter : OfferFlightsListContract.Presenter {
+
+    override fun getOfferFlights(basicFlight: BasicFlight) {
+    }
+
+    override fun getOfferFlightsByUrl(url: String, basicFlight: BasicFlight) {
+    }
+
+    override fun getData() {
+    }
+}
